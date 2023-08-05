cb460c2f82782230d3d91dd024d4b5a8f1d9c216
nowgnas <dev.nowgnas@gmail.com>
2023-08-05 15:46:10 +0900

    :art: Item2: singleton, builder pattern

---
+++ src/chapter2/item1/StaticMethod.java
@@ -0,0 +1,11 @@
+package chapter2.item1;
+
+public class StaticMethod {
+    public static Boolean valueOf(boolean b) {
+        return b ? Boolean.TRUE : Boolean.FALSE;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
